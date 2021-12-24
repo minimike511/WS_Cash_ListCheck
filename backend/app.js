23944653077c76903adabb57142e296727e50600
Michael <michael511.mp@gmail.com>
2021-12-24 18:34:50 -0500

    copy other project code as init

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const postsRoutes = require("./routes/post");
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/node-angular')
+  .then(() => {
+    console.log('Connected to Database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
